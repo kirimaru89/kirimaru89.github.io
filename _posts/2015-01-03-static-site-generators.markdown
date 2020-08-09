2890261e8945059e26ec9f471f35df5323e63906
Apple <>
2020-08-09 22:00:40 +0700

    xxx

---
+++ _posts/2015-01-03-static-site-generators.markdown
@@ -0,0 +1,8 @@
+---
+layout: single
+author: BCB
+title:  "Mot hai ba bon"
+date:   2020-07-08 21:57:58 +0700
+categories: jekyll update
+---
+The deo nao lai khong len
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "Mot hai ba bon",
+        "excerpt":"The deo nao lai khong len  ","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "http://localhost:4000/jekyll/update/static-site-generators/",
+        "teaser": null
+      },{
+        "title": "Xin chào tất cả các bạn",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "http://localhost:4000/jekyll/update/welcome-to-jekyll/",
+        "teaser": null
+      },{
+        "title": "Sony a7SIII – 5 năm đợi chờ đã được đền đáp",
+        "excerpt":"Tối qua theo giờ Việt Nam, Sony chính thức công bố chiếc máy ảnh full-frame thuộc dòng chuyên quay phim của họ, Sony a7SIII. Đây là thành quả kết tinh sau gần 5 năm kể từ người tiền nhiệm Sony a7SII Theo một nghiên cứu thị trường, tính đến năm...","categories": ["lap-trinh"],
+        "tags": [],
+        "url": "http://localhost:4000/lap-trinh/deo-hieu/",
+        "teaser": null
+      }]
